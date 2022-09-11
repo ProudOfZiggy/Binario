8875b3df49d33e733b11f58747adc13b845a9b4c
Niktia Rodionov <>
2022-09-12 02:05:52 +0900

    Use tar instead of zip

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ fi
 
 echo "Releasing version $VERSION of Binario"
 
-make clean
-make build-universal
-tar -cvzf ".build/binario.zip" -C ".build/apple/Products/Release" "./binario"
-gh release create "v"$VERSION ".build/binario.zip"
+# make clean
+# make build-universal
+tar -cvzf ".build/binario.tar.gz" -C ".build/apple/Products/Release" "./binario"
+gh release create "v"$VERSION ".build/binario.tar.gz"

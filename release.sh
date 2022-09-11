1b87607d7b35727c1b117755af634e876996a80c
Niktia Rodionov <>
2022-09-12 01:40:30 +0900

    Update archiving

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 make clean
 make build
-tar -cvzf ".build/binario.zip" ".build/apple/Products/Release/binario"
+tar -cvzf ".build/binario.zip" -C ".build/apple/Products/Release" "./binario"
 gh release create 0.80.5 ".build/binario.zip"

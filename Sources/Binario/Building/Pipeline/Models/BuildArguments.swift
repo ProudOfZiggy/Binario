b33286a9e2aa108c219485cee873e523e31ffb91
Ivan Morozov <allui.d.nest@gmail.com>
2023-01-27 10:54:54 +0500

    update BuildPipelineResolve

--- Sources/Binario/Building/Pipeline/Models/BuildArguments.swift
+++ Sources/Binario/Building/Pipeline/Models/BuildArguments.swift
@@ -29,8 +29,31 @@ class BuildCommandsBuilder {
          "-configuration", "Release",
          "-archivePath", "\(buildConfiguration.archivesPath)/Release-\(platform.rawValue)",
          "-destination", platform.destination,
+         "-derivedDataPath", "\(buildConfiguration.buildDirectory)",
          "BUILD_DIR=\(buildConfiguration.buildDirectory)",
          "SKIP_INSTALL=NO",
          "BUILD_LIBRARY_FOR_DISTRIBUTION=YES"]
+    }
+}
+
+class ResolveCommandBuilder {
+    struct Command {
+        let arguments: [String]
+    }
+
+    let buildConfiguration: PackageBuildConfiguration
+
+    init(buildConfiguration: PackageBuildConfiguration) {
+        self.buildConfiguration = buildConfiguration
+    }
+
+    func buildCommand() -> Command {
+        Command(arguments: [
+            "xcrun",
+            "xcodebuild",
+            "-resolvePackageDependencies",
+            "-scheme", "\(buildConfiguration.packageName)",
+            "-derivedDataPath", "\(buildConfiguration.buildDirectory)"
+        ])
     }
 }
